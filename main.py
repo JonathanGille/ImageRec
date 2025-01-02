f68ca63aa1e94f32d17a1501e5c36c19448ac090
JonathanGille <104131704+JonathanGille@users.noreply.github.com>
2025-01-02 13:28:21 +0100

    asdf

--- main.py
+++ main.py
@@ -18,15 +18,15 @@ vMRTs = get_images(vMRTs_folder)
 scans = get_images(scans_folder)
 
 
-def similarity_matrix(imgs1, imgs2):
+def similarity_matrix(imgs1, imgs2, model_name='efficientnet_b0'):
     df = pd.DataFrame(index=[img.name for img in imgs1], columns=[img.name for img in imgs2])
 
     for img1 in imgs1:
         for img2 in imgs2:
-            sim = image_similarity(img1, img2)
+            sim = image_similarity(img1, img2, model_name=model_name)
             df.loc[img1.name, img2.name] = sim
     
     return df
 
-df = similarity_matrix(scans, vMRTs[0:2])
+df = similarity_matrix(scans, vMRTs[0:2], model_name='efficientnet_b1')
 print(df)
